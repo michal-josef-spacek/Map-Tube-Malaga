f5664dde6955171606ef20bb7c5828514f5ff190
Michal Špaček <tupinek@gmail.com>
2015-11-02 18:36:27 +0100

    Add EXAMPLE5 section to doc.

--- Malaga.pm
+++ Malaga.pm
@@ -218,6 +218,51 @@ For more information about Málaga Map, click L<here|https://es.wikipedia.org/wi
  # Línea 1
  # Línea 2
 
+=head1 EXAMPLE5
+
+ # Pragmas.
+ use strict;
+ use warnings;
+
+ # Modules.
+ use Encode qw(decode_utf8 encode_utf8);
+ use Map::Tube::Malaga;
+
+ # Arguments.
+ if (@ARGV < 1) {
+         print STDERR "Usage: $0 line\n";
+         exit 1;
+ }
+ my $line = decode_utf8($ARGV[0]);
+
+ # Object.
+ my $obj = Map::Tube::Malaga->new;
+
+ # Get stations for line.
+ my $stations_ar = $obj->get_stations($line);
+
+ # Print out.
+ map { print encode_utf8($_->name)."\n"; } @{$stations_ar};
+
+ # Output:
+ # Usage: __PROG__ line
+
+ # Output with 'foo' argument.
+ # Map::Tube::get_stations(): ERROR: Invalid Line Name [foo]. (status: 105) file __PROG__ on line __LINE__
+
+ # Output with 'Línea 1' argument.
+ # El Perchel
+ # La Unión
+ # Barbarela
+ # Carranque
+ # Portada Alta
+ # Ciudad de la Justicia
+ # Universidad
+ # Clínico
+ # El Cónsul
+ # Paraninfo
+ # Andalucía Tech
+
 =head1 DEPENDENCIES
 
 L<File::Share>,
